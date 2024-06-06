cb776922dedad98618730d62029165c1b213dbe4
Lrn-java <625514232@qq.com>
2024-06-06 15:26:27 +0800

    Changes

--- src/main/java/HDFSClient.java
+++ src/main/java/HDFSClient.java
@@ -8,15 +8,16 @@ import static java.lang.System.out;
 
 public class HDFSClient {
     private static FileSystem hdfs;
+
     /**
      * 连接HDFS
      */
-    static{
+    static {
         try {
             URI uri = new URI("hdfs://192.168.137.129:9820");
             Configuration conf = new Configuration();
             String user = "root";
-            hdfs = FileSystem.get(uri,conf,user);
+            hdfs = FileSystem.get(uri, conf, user);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -24,20 +25,30 @@ public class HDFSClient {
 
     /**
      * 创建指定目录
+     *
      * @param path
      * @throws IOException
      */
-    public static void mkdir(String path) throws IOException{
-        hdfs.mkdirs(new Path(path));
+    public static boolean mkdir(String path) throws IOException {
+        if (hdfs.exists(new Path(path))){
+            out.println("该目录存在");
+
+        }else{
+            boolean access = hdfs.mkdirs(new Path(path));
+            out.println("创建" + (access ? "成功" : "失败") + ":" + path);
+        }
+        return false;
     }
 
     /**
      * 删除指定目录
+     *
      * @param path
      * @throws IOException
      */
-    public static void delete(String path) throws IOException {
-        hdfs.delete(new Path(path));
+    public static boolean delete(String path) throws IOException {
+        if (hdfs.exists(new Path(path)));
+        return false;
     }
-
 }
+
